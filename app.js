81e038b35c2d1f52d3b9f3f865aee3df690c2d78
Mayur hirpara <mayur@cloudcraftsmanship.io>
2023-01-18 11:58:18 +0530

    Optimize routes

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
-
-var authMiddleware = require('./middlewares/authMiddleware.controller');
+const routes = require('./routes/index');
 
 var app = express();
 
@@ -16,6 +11,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,10 +19,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/', indexRouter);
-app.use('/user', authMiddleware);//Auth middleware
-app.use('/user', usersRouter);
-app.use('/auth', authRouter);
+app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const usersRouter = require('./users');
+const authRouter = require('./auth');
 const homeController = require('../controllers/home.controller');
-/* GET home page. */
+const authMiddleware = require('../middlewares/authMiddleware.controller');
+
+// Home page
 router.get('/', homeController.index);
+
+// Users routes
+router.use('/user', authMiddleware);//Auth middleware
+router.use('/user', usersRouter);
+
+// Auth routes
+router.use('/auth', authRouter);
 
 module.exports = router;
